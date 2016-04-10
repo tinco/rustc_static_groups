d2c6521352110246cabd338129406c56a01829a4
Tinco Andringa <mail@tinco.nl>
2016-04-11 00:01:32 +0200

    start on compiler plugin

---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+#![crate_type="dylib"]
+#![feature(plugin_registrar, rustc_private)]
+
+extern crate syntax;
+extern crate rustc_plugin;
+
+use rustc_plugin::Registry;
+use syntax::ext::base::*;
+use syntax::codemap::*;
+use syntax::ast::*;
+
+pub fn get<T>(n: &str) -> Option<T> {
+	None
+}
+
+pub struct StaticGroupItemDecorator { x: bool }
+
+impl MultiItemDecorator for StaticGroupItemDecorator {
+	fn expand(&self, ecx: &mut ExtCtxt, sp: Span, meta_item: &MetaItem, item: &Annotatable, push: &mut FnMut(Annotatable)) {
+		match item {
+			&Annotatable::Item(really_item) => {
+				match really_item.node {
+					ItemKind::Fn(ref decl, ref style, ref constness, ref abi, ref generics, ref block) => {
+						// trace!("{}", decl);
+						// ...? Add something here.
+					}
+					_ => {
+						ecx.span_err(sp, "static_group is only permissiable on functions for now");
+					}
+				}
+			}
+			_ => {
+				ecx.span_err(sp, "static_group is only permissiable on functions for now");
+			}
+		}
+	}
+}
+
+#[plugin_registrar]
+pub fn plugin_registrar(reg: &mut Registry) {
+	use syntax::parse::token::intern;
+	use syntax::ext::base;
+
+	let static_group_item_decorator = Box::new(StaticGroupItemDecorator { x: true });
+
+	reg.register_syntax_extension(intern("static_group"), SyntaxExtension::MultiDecorator(static_group_item_decorator));
+}
+
+
+#[cfg(test)]
+mod tests {
+
+	type TestSignature = fn () -> bool;
+
+	#[static_group="my_statics"]
+	fn my_static() -> bool {
+		return true;
+	}
+
+	#[test]
+	fn it_works() {
+		match static_groups::get::<TestSignature>("my_statics") {
+			Some (my_fun) => assert!(my_fun()),
+			None => assert!(false),
+		};
+	}
+}
